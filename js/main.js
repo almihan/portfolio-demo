ffeb6144d589bbb209f8ef5863f0b55f5d19657c
almi adi <almmiree@gmail.com>
2021-06-06 17:37:57 -0400

    build about html page.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const menuBtn=document.querySelector(".btn-menu");
 const hamburger = document.querySelector(".btn-menu__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".nav-menu");
+const navItems = document.querySelectorAll('.nav-menu__item')
+
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -12,12 +14,14 @@ function toggleMenu(){
     hamburger.classList.add("open");
     nav.classList.add("open");
     menuNav.classList.add("open");
+    navItems.forEach(item => item.classList.add('open'));
     showMenu = true;
 
   }else{
     hamburger.classList.remove('open');
     nav.classList.remove("open");
     menuNav.classList.remove("open");
+    navItems.forEach(item => item.classList.remove('open'));
     showMenu=false;
   }
 
